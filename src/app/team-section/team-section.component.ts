8c99cacf325c0bb75c2d95978eea58055bda49cf
Robert Willemelis <robert.dev@gmx.de>
2023-04-02 20:22:40 +0200

    fix: team section responsiveness

--- src/app/team-section/team-section.component.ts
+++ src/app/team-section/team-section.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {MatDialog} from '@angular/material/dialog';
 import { AppComponent } from '../app.component';
 import {DialogPersonComponent} from '../dialog-person/dialog-person.component';
@@ -8,15 +8,27 @@ import {DialogPersonComponent} from '../dialog-person/dialog-person.component';
   templateUrl: './team-section.component.html',
   styleUrls: ['./team-section.component.scss']
 })
-export class TeamSectionComponent{
+export class TeamSectionComponent implements OnInit{
   @Input() image;
   @Input() team;
+  @Input()   lazy = false;
+  @Input()   height;
+  @Input()   fadeout = false;
   @Input() title: string;
   @Input() showPopups = false;
   team$;
+  responsive: boolean = false;
+  isMobile = false;
+  
+  loading = 'eager';
+  device: any;
+  mobileHeight: number = 700;
+  
+  extraClasses = '';
 
-  constructor(public dialog: MatDialog) {
-    console.log('constructro team section')
+  constructor(public dialog: MatDialog, private host:AppComponent) {
+    console.log('constructro team section');
+    this.isMobile = this.host.class.indexOf('desktop') === -1 ? true : false;
   }
   
   openDialog(person): void {
@@ -25,5 +37,18 @@ export class TeamSectionComponent{
       data: person,
       autoFocus: false
     });
+  }
+  ngOnInit(): void {
+    let imageHeight: string = '700';
+      this.loading = this.lazy === true ? 'lazy' : this.loading;
+      this.responsive = (this.image && this.image.responsive) ? this.image.responsive : this.responsive;
+      if(this.isMobile === true){
+        this.extraClasses += ' bg--mobile';
+      }
+      if(this.fadeout){
+        this.extraClasses += ' bg-image--fadout';
+      }
+      this.mobileHeight = (this.image && this.image.mobileHeight) ? this.image.mobileHeight : this.mobileHeight;
+      this.extraClasses += `bg-image--${imageHeight} bg-image-mobile--${this.mobileHeight}` 
   }
 }
